1cf5a8408d07ecdf067a10df31ddd60a330f5542
Luciano Dutra Cavalcante de Souza <lucianoo_dutra@hotmail.com>
2022-06-18 14:04:08 -0300

    atualização do portifólio, fazendo um do zero usando o react redux

---
+++ my-portifolio-reactredux/src/redux/reducers/index.js
@@ -0,0 +1,12 @@
+const INITIAL_STATE = {
+};
+
+const rootReducer = (state = INITIAL_STATE, action) => {
+  switch (action.type) {
+  default:
+    return state;
+  }
+  // return state;
+};
+
+export default rootReducer;
